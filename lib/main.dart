5ce283d0cf510d451006a339edd1e3e38a48c331
EsmaeiliSina <es.sina76@gmail.com>
2021-02-13 00:31:02 +0330

    challenges done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 1;
+  void Roll() {
+    setState(() {
+      left = Random().nextInt(6) + 1;
+      right = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  Roll();
+                },
+                child: Image.asset('images/dice$left.png'),
+              ),
+            ),
+            // SizedBox(
+            //   width: 10,
+            // ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  Roll();
+                },
+                child: Image.asset('images/dice$right.png'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
